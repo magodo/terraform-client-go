a1d676fa8a084f8226eb70402ea834e604109642
magodo <wztdyl@sina.com>
2023-03-13 18:19:24 +0800

    to/from proto add the derivation commit

--- tfclient/tfprotov5/internal/fromproto/fromproto.go
+++ tfclient/tfprotov5/internal/fromproto/fromproto.go
@@ -1,2 +1,2 @@
-// This is duplicated from github.com/hashicorp/terraform-plugin-go/tfprotov5/internal/fromproto
+// This is duplicated from github.com/hashicorp/terraform-plugin-go/tfprotov5/internal/fromproto (15ecdb66c84cd8202b0ae3d34c44cb4bbece5444)
 package fromproto
--- tfclient/tfprotov5/internal/toproto/toproto.go
+++ tfclient/tfprotov5/internal/toproto/toproto.go
@@ -1,2 +1,2 @@
-// This is duplicated from github.com/hashicorp/terraform-plugin-go/tfprotov5/internal/toproto
+// This is duplicated from github.com/hashicorp/terraform-plugin-go/tfprotov5/internal/toproto (15ecdb66c84cd8202b0ae3d34c44cb4bbece5444)
 package toproto
